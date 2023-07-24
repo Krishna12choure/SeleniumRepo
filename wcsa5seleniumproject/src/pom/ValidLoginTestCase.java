c4a8d621266b7a080781191038bfc333473879a8
Krishna <Krishna@DESKTOP-6PV51OG>
2023-07-24 17:27:47 +0530

    Commit

--- wcsa5seleniumproject/src/pom/ValidLoginTestCase.java
+++ wcsa5seleniumproject/src/pom/ValidLoginTestCase.java
@@ -12,13 +12,12 @@ public class ValidLoginTestCase extends BaseTest{
 		
 		
 		BaseTest bt = new BaseTest();
-		bt.openBrowser();
 		
 		Flib flib = new Flib();
 		driver.findElement(By.name("username")).sendKeys(flib.readPropertyData(PROP_PATH, "Username"));
 		driver.findElement(By.name("pwd")).sendKeys(flib.readPropertyData(PROP_PATH, "Password"));
 		driver.findElement(By.id("loginButton")).click();
-		bt.closeBrowser();
+		
 	}
 
 }
